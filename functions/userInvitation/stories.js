54ed5a7870043cd53adebac60950ee04fb60371f
David Baumgold <david@impraise.com>
2018-01-26 14:21:53 +0100

    Storybook Knobs

--- functions/userInvitation/stories.js
+++ functions/userInvitation/stories.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text } from "@storybook/addon-knobs/react";
 import Template from "./src/template";
 import textTemplate from "./src/template.text";
 
 storiesOf("userInvitation", module)
+  .addDecorator(withKnobs)
   .add("HTML", () => {
     const data = {
-      baseUrl: "example-website.com",
+      baseUrl: text("baseUrl", "example-website.com"),
       user: {
-        email: "foo@example.com",
+        email: text("user email", "foo@example.com"),
         invitationToken: "abc123"
       },
       invitedBy: {
-        name: "Sam Smith"
+        name: text("invitedBy name", "Sam Smith")
       },
       group: {
-        name: "Cool Kids"
+        name: text("group name", "Cool Kids")
       }
     };
     return <Template {...data} />;
--- functions/userReminder/stories.js
+++ functions/userReminder/stories.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text, date } from "@storybook/addon-knobs/react";
 import { IntlProvider } from "react-intl";
 import Template from "./src/template";
 import textTemplate from "./src/template.text";
 
+function isoDate(name, defaultValue) {
+  const stringTimestamp = date(name, new Date(defaultValue));
+  return new Date(stringTimestamp).toISOString();
+}
+
 storiesOf("userReminder", module)
+  .addDecorator(withKnobs)
   .add("HTML", () => {
     const data = {
-      baseUrl: "example-website.com",
+      baseUrl: text("baseUrl", "example-website.com"),
       user: {
-        email: "foo@example.com",
-        firstName: "Test",
+        email: text("user email", "foo@example.com"),
+        firstName: text("user firstName", "Alice"),
         locale: "en",
-        timeZone: "America/New_York"
+        timeZone: text("user timeZone", "America/New_York"),
       },
       event: {
         id: 1,
-        title: "Pool Party",
-        startAt: "2017-02-01T10:00:00Z",
-        endAt: "2017-02-01T22:00:00Z"
+        title: text("event title", "Pool Party"),
+        startAt: isoDate("event startAt", "2017-02-01T10:00:00Z"),
+        endAt: isoDate("event endAt", "2017-02-01T22:00:00Z"),
       }
     };
     return (
