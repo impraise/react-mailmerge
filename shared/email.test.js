f5a5e6456373b4433fd1b9990827396d4d16f150
David Baumgold <david@davidbaumgold.com>
2018-02-17 11:52:35 +0100

    100% coverage!

--- shared/email.test.js
+++ shared/email.test.js
@@ -1,20 +1,26 @@
 describe("email client", () => {
-  let mockClient, mockSendEmail, Client, environmentEnv;
+  let consoleErr, environmentEnv, mockClient, mockSendEmail, Client;
 
   beforeEach(() => {
+    consoleErr = global.console.error;
+    environmentEnv = process.env.ENVIRONMENT;
+
     jest.mock("postmark", () => {
       mockClient = jest.fn().mockReturnThis();
       mockSendEmail = jest.fn((payload, callback) =>
         callback(null, { origin: "test" })
       );
+      mockSendEmail.returnError = err => {
+        mockSendEmail.mockImplementation((payload, callback) => callback(err));
+      };
       mockClient.sendEmail = mockSendEmail;
       return { Client: jest.fn(() => mockClient) };
     });
     Client = require("./email").default;
-    environmentEnv = process.env.ENVIRONMENT;
   });
 
   afterEach(() => {
+    global.console.error = consoleErr;
     process.env.ENVIRONMENT = environmentEnv;
   });
 
@@ -51,6 +57,29 @@ describe("email client", () => {
         Subject: "'foo@example.com': Test"
       },
       expect.any(Function)
+    );
+  });
+
+  it("logs errors", () => {
+    const payload = {
+      To: "foo@example.com",
+      Subject: "Test"
+    };
+    process.env.ENVIRONMENT = "staging";
+    global.console.error = jest.fn();
+    mockSendEmail.returnError({ message: "whoops" });
+    const client = new Client("token");
+    client.sendEmail(payload);
+
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      {
+        To: "mailmerge@example.com",
+        Subject: "'foo@example.com': Test"
+      },
+      expect.any(Function)
+    );
+    expect(global.console.error).toHaveBeenCalledWith(
+      "Unable to send via postmark: whoops"
     );
   });
 });
